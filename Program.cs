54bad3fe41e7d68335237b1b3f259adb5b1cb641
Jorge Lucas <lukasrochaaraujo@hotmail.com>
2021-07-05 10:16:57 -0300

    Update program with queries.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace dapper_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var connection = new SQLiteConnection("Data Source=.\\Company.db; Version=3");
+            
+            var bookList = connection.Query<Book>("SELECT * FROM Book").ToList();
+            var authorWithBookList = connection.Query<Author, Book, Author>("SELECT * FROM Author a LEFT JOIN Book b on b.AuthorId = a.Id",
+                                                (author, book) => {
+                                                    author.Books.Add(book);
+                                                    return author;
+                                                })
+                                                .ToList();
+            var cleanCodeBook = connection.Query<Book>("SELECT * FROM Book where Title = @Title", new { Title = "Clean Code" }).First();
+
+            bookList.ForEach(book => 
+                Console.WriteLine(book));
+
+            authorWithBookList.ForEach(author => 
+                Console.WriteLine(author));
+
+            Console.WriteLine(cleanCodeBook);
+        }
+    }
+}
